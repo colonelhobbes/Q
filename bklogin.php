602fa8a9ae42cd0296be3ca2ff99ed1fbf586656
Colonelhobbes <arun.kal927@gmail.com>
2015-10-10 06:01:05 -0700

    Woo

--- bklogin.php
+++ bklogin.php
@@ -1,3 +1,19 @@
 <?
+if($_POST){
+$database_name = "eventDB";
+$mysql_host = "localhost"; //almost always 'localhost'
+$database_user = "root";
+$database_pwd = "root";
+$dbc = mysql_connect($mysql_host, $database_user, $database_pwd);
+if(!$dbc)
+{
+    die("We are currently experiencing very heavy traffic to our site, please be patient and try again shortly.");
+}
+$db = mysql_select_db($database_name);
+
+$sql = "SELECT id FROM event";
+$res = mysql_query($sql);
+if()
+
 
 ?>
