e8cbeb786b972fc312b7504a07facd38b3c554f9
satej1210 <satej1210@live.com>
2015-10-10 15:53:01 -0700

    deploying azure db

--- submit.php
+++ submit.php
@@ -2,15 +2,15 @@
 
 if($_POST){
 $database_name = "eventDB";
-$mysql_host = "localhost"; //almost always 'localhost'
-$database_user = "root";
-$database_pwd = "root";
-$dbc = mysql_connect($mysql_host, $database_user, $database_pwd);
+$mysql_host = "nmbzrmx555.database.windows.net:1433"; //almost always 'localhost'
+$database_user = "eecs";
+$database_pwd = "IntelNUC777";
+$dbc = mssql_connect($mysql_host, $database_user, $database_pwd);
 if(!$dbc)
 {
     die("We are currently experiencing very heavy traffic to our site, please be patient and try again shortly.");
 }
-$db = mysql_select_db($database_name);
+$db = mssql_select_db($database_name);
 $i=0;
 if($_POST['remote_access']!=0)
     $i=1;
@@ -20,9 +20,9 @@ if(!$db)
 }
 $sql = "insert into event(eventName, rmtAcc, smsP, avgWaitTime, closeF)
 values('".$_POST['event_name']."', '".$i."', '".$_POST['SMS_parameters']."', '".$_POST['avg_wt']."', '".$_POST['closef']."');";
-$res = mysql_query($sql);
+$res = mssql_query($sql);
 $sql1 = "insert into passwords(passwordEnc) values(MD5('".($_POST['password'])."'));";
-$res1 = mysql_query($sql1);
+$res1 = mssql_query($sql1);
 echo $res1['_msg'];
 echo 'Thank you for your entry!';
 }
