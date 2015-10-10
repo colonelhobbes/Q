677051a94b91e420e629b9847aed1a5d06b17bbe
Colonelhobbes <arun.kal927@gmail.com>
2015-10-10 05:49:35 -0700

    Woo

---
+++ submit.php
@@ -0,0 +1,32 @@
+<?
+if($_POST){
+$database_name = "eventDB";
+$mysql_host = "localhost"; //almost always 'localhost'
+$database_user = "root";
+$database_pwd = "root";
+$dbc = mysql_connect($mysql_host, $database_user, $database_pwd);
+if(!$dbc)
+{
+    die("We are currently experiencing very heavy traffic to our site, please be patient and try again shortly.");
+}
+$db = mysql_select_db($database_name);
+$i=0;
+if($_POST['remote_access']!=0)
+    $i=1;
+if(!$db)
+{
+    die("Failed to connect to database - check your database name.");
+}
+$sql = "insert into event(eventName, rmtAcc, smsP, avgWaitTime, closeF)
+values('".$_POST['event_name']."', '".$i."', '".$_POST['SMS_parameters']."', '".$_POST['avg_wt']."', '".$_POST['closef']."');";
+$res = mysql_query($sql);
+echo 'This is your submission : <table><tr>';
+echo '<td>'.$_POST['eventName'].'</td>';
+echo '<td>'.$_POST['clientName'].'</td>';
+echo '<td>'.$_POST['remote_access'].'</td>';
+echo '<td>'.$_POST['SMS_Parameters'].'</td>';
+echo '<td>'.$_POST['avg_wt'].'</td>';
+echo '<td>'.$_POST['closef'].'</td>'; 
+echo '</tr></table>';
+}
+?>
