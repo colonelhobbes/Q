e91b4a6dfe18635b9df5f74991e7678d541f82b8
satej1210 <satej1210@live.com>
2015-10-11 01:26:33 -0700

    almost final4

--- AdminGuiPhp.php
+++ AdminGuiPhp.php
@@ -1,4 +1,29 @@
-<?
+<? 
+function SendData()
+{
+    try
+    {
+        $serverName = "tcp:nmbzrmx555.database.windows.net,1433";
+        $connectionOptions = array("Database"=>"eventDB",
+            "Uid"=>"eecs", "PWD"=>"IntelNUC777");
+        $conn = sqlsrv_connect($serverName, $connectionOptions);
+        if($conn == false)
+            die(FormatErrors(sqlsrv_errors()));
+        $tsql = "select clientName, clientAreaCode, clientPhone, position, passwords.id, passwordEnc from passwords inner join client on client.eventID = passwords.id and passwords.passwordEnc='" . $_POST['password'] . "';";
+        $insertReview = sqlsrv_query($conn, $tsql);
+        if($insertReview == FALSE)
+            die(sqlsrv_errors());
+        PrintHTML($row);
+        sqlsrv_free_stmt($insertReview);
+        sqlsrv_close($conn);
+    }
+    catch(Exception $e)
+    {
+        echo("Error!");
+    }
+}
+function PrintHTML($row)
+{
 echo ('<!DOCTYPE html>
 <html>
   <head>
@@ -6,30 +31,10 @@ echo ('<!DOCTYPE html>
     <link rel = "stylesheet" type = "text/css" href = "AdminGuiStyle.css" >
   </head>
 ');
-if(isset($_POST['submit'])){ //check if form was submitted
-$input = $_POST['password']; //get input text
-echo "Success! You enstered: ".$input;
-$database_name = "eventDB";
-$mysql_host = "localhost"; //almost always 'localhost'
-$database_user = "root";
-$database_pwd = "root";
-$dbc = mysql_connect($mysql_host, $database_user, $database_pwd);
-if(!$dbc)
-{
-    die("We are currently experiencing very heavy traffic to our site, please be patient and try again shortly.");
-}
-$db = mysql_select_db($database_name);
-if(!$db){
-	echo ('not valid db');
-}
-$md5 = md5($_POST['password']);
-$sql = "select clientName, clientAreaCode, clientPhone, position, passwords.id, passwordEnc from passwords inner join client on client.eventID = passwords.id and passwords.passwordEnc='".$md5."';";
-$res = mysql_query($sql);
-echo(mysql_error());
-if(mysql_num_rows($res) != 0){
-	echo('valid');
-echo('''<body style = "font-family: Verdana">
-  <h1 style = "text.align: center"> '. $row['eventName'].' </h1>
+    if (sqlsrv_num_rows($res) != 0)
+		{
+		echo ('''<body style = "font-family: Verdana">
+  <h1 style = "text.align: center"> ' . $row['eventName'] . ' </h1>
   <table style="width:95%; border-spacing: 0px;">
     <thead style = "background-color: #E3A20B; height = 35px">
       <th>Queue #</th>
@@ -38,10 +43,11 @@ echo('''<body style = "font-family: Verdana">
       <th>Phone # </th>
     </thead>
     <tbody>''');
-while($row = mysql_fetch_assoc($res)) {
-
-    if ($row['position']%2==0){
-      echo ('''<tr style = "/* Permalink - use to edit and share this gradient: http://colorzilla.com/gradient-editor/#1e5799+0,207cca+11,207cca+11,2989d8+50,7db9e8+100 */
+		while ($row = sqlsrv_fetch_assoc($res))
+			{
+			if ($row['position'] % 2 == 0)
+				{
+				echo ('''<tr style = "/* Permalink - use to edit and share this gradient: http://colorzilla.com/gradient-editor/#1e5799+0,207cca+11,207cca+11,2989d8+50,7db9e8+100 */
 background: #1e5799; /* Old browsers */
 background: -moz-linear-gradient(left,  #1e5799 0%, #207cca 11%, #207cca 11%, #2989d8 50%, #7db9e8 100%); /* FF3.6+ */
 background: -webkit-gradient(linear, left top, right top, color-stop(0%,#1e5799), color-stop(11%,#207cca), color-stop(11%,#207cca), color-stop(50%,#2989d8), color-stop(100%,#7db9e8)); /* Chrome,Safari4+ */
@@ -50,9 +56,10 @@ background: -o-linear-gradient(left,  #1e5799 0%,#207cca 11%,#207cca 11%,#2989d8
 background: -ms-linear-gradient(left,  #1e5799 0%,#207cca 11%,#207cca 11%,#2989d8 50%,#7db9e8 100%); /* IE10+ */
 background: linear-gradient(to right,  #1e5799 0%,#207cca 11%,#207cca 11%,#2989d8 50%,#7db9e8 100%); /* W3C */
 filter: progid:DXImageTransform.Microsoft.gradient( startColorstr="#1e5799", endColorstr="#7db9e8",GradientType=1 ); /* IE6-9 ">''');
-    }
-    else {
-      echo ('<tr style = "/* Permalink - use to edit and share this gradient: http://colorzilla.com/gradient-editor/#398235+0,398235+0,8ab66b+44,c9de96+100 */
+				}
+			  else
+				{
+				echo ('<tr style = "/* Permalink - use to edit and share this gradient: http://colorzilla.com/gradient-editor/#398235+0,398235+0,8ab66b+44,c9de96+100 */
 background: #398235; /* Old browsers */
 background: -moz-linear-gradient(left,  #398235 0%, #398235 0%, #8ab66b 44%, #c9de96 100%); /* FF3.6+ */
 background: -webkit-gradient(linear, left top, right top, color-stop(0%,#398235), color-stop(0%,#398235), color-stop(44%,#8ab66b), color-stop(100%,#c9de96)); /* Chrome,Safari4+ */
@@ -62,25 +69,34 @@ background: -ms-linear-gradient(left,  #398235 0%,#398235 0%,#8ab66b 44%,#c9de96
 background: linear-gradient(to right,  #398235 0%,#398235 0%,#8ab66b 44%,#c9de96 100%); /* W3C */
 filter: progid:DXImageTransform.Microsoft.gradient( startColorstr="#398235", endColorstr="#c9de96",GradientType=1 ); /* IE6-9 */
 ">');
-    }
+				}
 
-    echo ('<td>'.$row['position'].'</td>');
-    echo ('<td>'.$row['clientName'].'</td>');
-    echo ('<td>'.$row['clientAreaCode'].'</td>');
-    echo ('<td>'.$row['clientPhone'].'</td>');
-    echo('</tr>');
-}
-}
-else{
-        echo("Invalid creds");
-}
-}
-echo('
+			echo ('<td>' . $row['position'] . '</td>');
+			echo ('<td>' . $row['clientName'] . '</td>');
+			echo ('<td>' . $row['clientAreaCode'] . '</td>');
+			echo ('<td>' . $row['clientPhone'] . '</td>');
+			echo ('</tr>');
+			}
+		}
+	  else
+		{
+		echo ("Invalid creds");
+		}
+	}
+
+echo ('
   </tbody>
 </table>
 </body>
 </html>
 ');
+}
+
+
+if (isset($_POST['submit']))
+	{ //check if form was submitted
+	ReadData();
+}
 ?>
 
 <html>
