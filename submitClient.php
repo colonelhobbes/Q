0605baaf03c8813cc8869c110a83539f0f246043
Colonelhobbes <arun.kal927@gmail.com>
2015-10-10 22:00:32 -0700

    test 7

--- submitClient.php
+++ submitClient.php
@@ -29,7 +29,9 @@ function ReadData()
     {
         echo("Error!");
     }
-        $tsql = "INSERT client(clientName, clientAreaCode, eventName, clientPhone, eventID) values('".$_POST['name']."', ".$_POST['area_code'].", '".$_POST['eventName']."', ".$_POST['phone_number'].", ".$_POST['eventID'].");";
+        $tsql = "INSERT client(clientName, clientAreaCode, eventName, clientPhone, eventID) 
+        values('".$_POST['name']."', ".$_POST['area_code'].", '".$_POST['eventName'].
+        	"', ".$_POST['phone_number'].", ".$_POST['eventID'].");";
     var_dump($_POST);
         $insertReview = sqlsrv_query($conn, $tsql);
         if($insertReview == FALSE)
