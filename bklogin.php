00373358c71682d06eb2796cd5ceaa7c12da98ac
satej1210 <satej1210@live.com>
2015-10-10 18:04:48 -0700

    Proper

--- bklogin.php
+++ bklogin.php
@@ -1,50 +1,50 @@
-<?    
+<?
 if(isset($_POST['submit'])){ //check if form was submitted
-$input = $_POST['password']; //get input text
-echo "Success! You enstered: ".$input;
-$database_name = "eventDB";
-$mysql_host = "localhost"; //almost always 'localhost'
-$database_user = "root";
-$database_pwd = "root";
-$dbc = mysql_connect($mysql_host, $database_user, $database_pwd);
-   
-if(!$dbc)
-{
-    die("We are currently experiencing very heavy traffic to our site, please be patient and try again shortly.");
-}
-$db = mysql_select_db($database_name);
-if(!$db){
-	echo ('not valid db');
-}
-$md5 = md5($_POST['password']);
-$sql = "select passwords.id, passwordEnc from passwords, event where event.id = passwords.id and passwords.passwordEnc='".$md5."';";
-$res = mysql_query($sql);
-echo(mysql_error());
-
-if(mysql_num_rows($res) != 0){
-	echo('valid');
-
-
-
-
-while($row = mysql_fetch_assoc($res)) {
- 
-    echo $row['id'].'<br />';
+    $input = $_POST['password']; //get input text
+    echo "Success! You enstered: ".$input;
+    $database_name = "eventDB";
+    $mysql_host = "localhost"; //almost always 'localhost'
+    $database_user = "root";
+    $database_pwd = "root";
+    $dbc = mysql_connect($mysql_host, $database_user, $database_pwd);
     
-}
-}
-
-else{
+    if(!$dbc)
+    {
+        die("We are currently experiencing very heavy traffic to our site, please be patient and try again shortly.");
+    }
+    $db = mysql_select_db($database_name);
+    if(!$db){
+        echo ('not valid db');
+    }
+    $md5 = md5($_POST['password']);
+    $sql = "select passwords.id, passwordEnc from passwords, event where event.id = passwords.id and passwords.passwordEnc='".$md5."';";
+    $res = mysql_query($sql);
+    echo(mysql_error());
+    
+    if(mysql_num_rows($res) != 0){
+        echo('valid');
+        
+        
+        
+        
+        while($row = mysql_fetch_assoc($res)) {
+            
+            echo $row['id'].'<br />';
+            
+        }
+    }
+    
+    else{
         echo("Invalid creds");
-}
+    }
 }
 ?>
 
 <html>
 <body>    
 <form action="" method="post">
-  <input type="text" name="password"/>
-  <input type="submit" name="submit"/>
+<input type="text" name="password"/>
+<input type="submit" name="submit"/>
 </form>    
 </body>
 </html>
--- submit.php
+++ submit.php
@@ -1,39 +1,15 @@
 <?
 if($_POST){
-
- $serverName = "tcp:nmbzrmx555.database.windows.net,1433";
-   $userName = 'eecs@nmbzrmx555';
-   $userPassword = 'IntelNUC777';
-   $dbName = "eventDB";
-   $table = "event";
-   $server = "tcp:nmbzrmx555.database.windows.net,1433";
-$user = "eecs@nmbzrmx555";
-$pwd = "IntelNUC777";
-$db = "eventDB";
-$sUsername = 'eecs@nmbzrmx555';
-$sPassword = 'IntelNUC777';
-$sHost = 'tcp:nmbzrmx555.database.windows.net,1433';
-$sDb = 'eventDB';
-$dsn = "sqlsrv:Server=$server;Database=$db";
-try {
-    $oConn = new PDO('mssql:host='.$sHost.';dbname='.$sDb, $sUsername, $sPassword);
-    $oConn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-    $oStmt = $oConn->prepare('SELECT * FROM events');
-    $oResult = $oStmt->fetchAll();
-
-    foreach ($oResult as $aRow) {
-        print_r($aRow['data']);
-    }
-
-} catch(PDOException $e) {
-    echo 'ERROR: ' . $e->getMessage();
-}
+$database_name = "eventDB";
+$mysql_host = "localhost"; //almost always 'localhost'
+$database_user = "root";
+$database_pwd = "root";
+$dbc = mysql_connect($mysql_host, $database_user, $database_pwd);
 if(!$dbc)
 {
-    die("We are curresntly experiencing very heavy traffic to our site, please be patient and try again shortly.");
+    die("We are currently experiencing very heavy traffic to our site, please be patient and try again shortly.");
 }
-$db = mssql_select_db($database_name);
+$db = mysql_select_db($database_name);
 $i=0;
 if($_POST['remote_access']!=0)
     $i=1;
@@ -43,9 +19,9 @@ if(!$db)
 }
 $sql = "insert into event(eventName, rmtAcc, smsP, avgWaitTime, closeF)
 values('".$_POST['event_name']."', '".$i."', '".$_POST['SMS_parameters']."', '".$_POST['avg_wt']."', '".$_POST['closef']."');";
-$res = mssql_query($sql);
+$res = mysql_query($sql);
 $sql1 = "insert into passwords(passwordEnc) values(MD5('".($_POST['password'])."'));";
-$res1 = mssql_query($sql1);
+$res1 = mysql_query($sql1);
 echo $res1['_msg'];
 echo 'Thank you for your entry!';
 }
