2652e0a6bd2fb0c5a58c18bc879ed6468a39c59e
satej1210 <satej1210@live.com>
2015-10-11 09:23:25 -0700

    final

--- deleteFirst.php
+++ deleteFirst.php
@@ -11,7 +11,7 @@ function Delete()
         $conn = sqlsrv_connect($serverName, $connectionOptions);
         if($conn == false)
             die((sqlsrv_errors()));
-        $tsql = "delete from client where position in (Select Min(Position) from client where client.eventID=event.id)";
+        $tsql = "delete from client where order by position LIMIT 1";
         $insertReview = sqlsrv_query($conn, $tsql);
         if($insertReview == FALSE)
             die(sqlsrv_errors());
