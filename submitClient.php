f89f240739a092926e5dd75d25ae399e88e085c5
Colonelhobbes <arun.kal927@gmail.com>
2015-10-10 20:21:32 -0700

    Azure

--- submitClient.php
+++ submitClient.php
@@ -1,23 +1,23 @@
 <?
 if($_POST){
 $database_name = "eventDB";
-$mysql_host = "localhost"; //almost always 'localhost'
-$database_user = "root";
-$database_pwd = "root";
-$dbc = mysql_connect($mysql_host, $database_user, $database_pwd);
+$mysql_host = "nmbzrmx555.database.windows.net,1433"; //almost always 'localhost'
+$database_user = "eecs";
+$database_pwd = "IntelNUC777";
+$dbc = mssql_connect($mysql_host, $database_user, $database_pwd);
 if(!$dbc)
 {
     die("We are currently experiencing very heavy traffic to our site, please be patient and try again shortly.");
 }
-$db = mysql_select_db($database_name);
+$db = mssql_select_db($database_name);
 if(!$db)
 {
     die("Failed to connect to database - check your database name.");
 }
 $sql = "insert into client(clientName, clientAreaCode, eventName, clientPhone, eventID)
 values('".$_POST['name']."', '".$_POST['area_code']."', '".$_POST['eventName']."', '".$_POST['phone_number']."', '".$_POST['eventID']."');";
-$res = mysql_query($sql);
-echo(mysql_error());
+$res = mssql_query($sql);
+
 echo 'Thank you for your entry!';
 }
 else
