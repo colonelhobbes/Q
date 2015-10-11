9bd0a283b271ca0fbf4a6837d0807181b8787775
satej1210 <satej1210@live.com>
2015-10-11 10:47:03 -0700

    final5

--- sendNotif.php
+++ sendNotif.php
@@ -1,10 +1,36 @@
+
 <?php
-// the message
-$msg = "Notification";
+require 'PHPMailerAutoload.php';
 
-// use wordwrap() if lines are longer than 70 characters
-$msg = wordwrap($msg,70);
+$mail = new PHPMailer;
 
-// send email
-mail("satej1210@live.com","My subject",$msg);
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'polize9876@gmail.com';                 // SMTP username
+$mail->Password = 'Password123-456';                           // SMTP password
+$mail->SMTPSecure = 'tls';                            // Enable encryption, 'ssl' also accepted
+
+$mail->From = 'polize9876@gmail.com';
+$mail->FromName = 'SwagNotifier';
+$mail->addAddress('mudit2103@gmail.com', 'Joe User');     // Add a recipient           // Name is optional
+//$mail->addReplyTo('info@example.com', 'Information');
+//$mail->addCC('cc@example.com');
+//$mail->addBCC('bcc@example.com');
+
+$mail->WordWrap = 50;                                 // Set word wrap to 50 characters
+//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = 'Here is the subject';
+$mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+if(!$mail->send()) {
+    echo 'Message could not be sent.';
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
+} else {
+    echo 'Message has been sent';
+}
 ?>
