769be8043a62af264f970385d8a4b67b3f6e407d
11rohans <11rohans@gmail.com>
2015-10-10 23:35:26 -0700

    Update AdminGuiPhp.php

--- AdminGuiPhp.php
+++ AdminGuiPhp.php
@@ -1,10 +1,6 @@
-
-
 <?
-
 echo ('<!DOCTYPE html>
 <html>
-
   <head>
     <title> Admin GUI </title>
     <link rel = "stylesheet" type = "text/css" href = "AdminGuiStyle.css" >
@@ -18,7 +14,6 @@ $mysql_host = "localhost"; //almost always 'localhost'
 $database_user = "root";
 $database_pwd = "root";
 $dbc = mysql_connect($mysql_host, $database_user, $database_pwd);
-
 if(!$dbc)
 {
     die("We are currently experiencing very heavy traffic to our site, please be patient and try again shortly.");
@@ -28,25 +23,47 @@ if(!$db){
 	echo ('not valid db');
 }
 $md5 = md5($_POST['password']);
-$sql = "select clientName, clientAreaCode, clientPhone, position, passwords.id, passwordEnc from passwords inner join client on client.eventID = passwords.id and passwords.passwordEnc='".$md5."' ORDER BY client.position;";
+$sql = "select clientName, clientAreaCode, clientPhone, position, passwords.id, passwordEnc from passwords inner join client on client.eventID = passwords.id and passwords.passwordEnc='".$md5."';";
 $res = mysql_query($sql);
 echo(mysql_error());
 if(mysql_num_rows($res) != 0){
 	echo('valid');
-
-echo('<body>
-  <h1> '. $row['eventName'].' </h1>
-  <table style="width:100%">
-    <thead>
+echo('''<body style = "font-family: Verdana">
+  <h1 style = "text.align: center"> '. $row['eventName'].' </h1>
+  <table style="width:95%; border-spacing: 0px;">
+    <thead style = "background-color: #E3A20B; height = 35px">
       <th>Queue #</th>
       <th>Name</th>
       <th>Area Code </th>
       <th>Phone # </th>
     </thead>
-    <tbody>');
+    <tbody>''');
 while($row = mysql_fetch_assoc($res)) {
 
-    echo ('<tr style = "background-color: DarkSeaGreen">');
+    if ($row['position']%2==0){
+      echo ('''<tr style = "/* Permalink - use to edit and share this gradient: http://colorzilla.com/gradient-editor/#1e5799+0,207cca+11,207cca+11,2989d8+50,7db9e8+100 */
+background: #1e5799; /* Old browsers */
+background: -moz-linear-gradient(left,  #1e5799 0%, #207cca 11%, #207cca 11%, #2989d8 50%, #7db9e8 100%); /* FF3.6+ */
+background: -webkit-gradient(linear, left top, right top, color-stop(0%,#1e5799), color-stop(11%,#207cca), color-stop(11%,#207cca), color-stop(50%,#2989d8), color-stop(100%,#7db9e8)); /* Chrome,Safari4+ */
+background: -webkit-linear-gradient(left,  #1e5799 0%,#207cca 11%,#207cca 11%,#2989d8 50%,#7db9e8 100%); /* Chrome10+,Safari5.1+ */
+background: -o-linear-gradient(left,  #1e5799 0%,#207cca 11%,#207cca 11%,#2989d8 50%,#7db9e8 100%); /* Opera 11.10+ */
+background: -ms-linear-gradient(left,  #1e5799 0%,#207cca 11%,#207cca 11%,#2989d8 50%,#7db9e8 100%); /* IE10+ */
+background: linear-gradient(to right,  #1e5799 0%,#207cca 11%,#207cca 11%,#2989d8 50%,#7db9e8 100%); /* W3C */
+filter: progid:DXImageTransform.Microsoft.gradient( startColorstr="#1e5799", endColorstr="#7db9e8",GradientType=1 ); /* IE6-9 ">''');
+    }
+    else {
+      echo ('<tr style = "/* Permalink - use to edit and share this gradient: http://colorzilla.com/gradient-editor/#398235+0,398235+0,8ab66b+44,c9de96+100 */
+background: #398235; /* Old browsers */
+background: -moz-linear-gradient(left,  #398235 0%, #398235 0%, #8ab66b 44%, #c9de96 100%); /* FF3.6+ */
+background: -webkit-gradient(linear, left top, right top, color-stop(0%,#398235), color-stop(0%,#398235), color-stop(44%,#8ab66b), color-stop(100%,#c9de96)); /* Chrome,Safari4+ */
+background: -webkit-linear-gradient(left,  #398235 0%,#398235 0%,#8ab66b 44%,#c9de96 100%); /* Chrome10+,Safari5.1+ */
+background: -o-linear-gradient(left,  #398235 0%,#398235 0%,#8ab66b 44%,#c9de96 100%); /* Opera 11.10+ */
+background: -ms-linear-gradient(left,  #398235 0%,#398235 0%,#8ab66b 44%,#c9de96 100%); /* IE10+ */
+background: linear-gradient(to right,  #398235 0%,#398235 0%,#8ab66b 44%,#c9de96 100%); /* W3C */
+filter: progid:DXImageTransform.Microsoft.gradient( startColorstr="#398235", endColorstr="#c9de96",GradientType=1 ); /* IE6-9 */
+">');
+    }
+
     echo ('<td>'.$row['position'].'</td>');
     echo ('<td>'.$row['clientName'].'</td>');
     echo ('<td>'.$row['clientAreaCode'].'</td>');
@@ -61,7 +78,6 @@ else{
 echo('
   </tbody>
 </table>
-
 </body>
 </html>
 ');
