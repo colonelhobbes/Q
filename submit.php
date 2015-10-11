06b149457c47b77b5533ff5ecdf1e871d7c8f11e
satej1210 <satej1210@live.com>
2015-10-11 10:25:45 -0700

    final4

--- submit.php
+++ submit.php
@@ -21,12 +21,12 @@ function SendData()
         $insertReview = sqlsrv_query($conn, $sql1);
         if($insertReview == FALSE)
             die(sqlsrv_errors());
-        $sql1 = "select id from event where eventName=".$_POST['event_name'].";";
+        $sql11 = "select id from event where eventName=".$_POST['event_name'].";";
         
-        $insertReview = sqlsrv_query($conn, $sql1);
-        $row = sqlsrv_fetch_array($insertReview, SQLSRV_FETCH_ASSOC);
+        $insertReview1 = sqlsrv_query($conn, $sql11);
+        $row = sqlsrv_fetch_array($insertReview1, SQLSRV_FETCH_ASSOC);
         var_dump( $row);
-        if($insertReview == FALSE)
+        if($insertReview1 == FALSE)
             die(sqlsrv_errors());
         sqlsrv_free_stmt($insertReview);
         sqlsrv_close($conn);
