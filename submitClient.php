d5d1657be3cc96d880a7a3f9fbdd5f29ad7ad9ce
Colonelhobbes <arun.kal927@gmail.com>
2015-10-10 20:56:34 -0700

    azure trial1

--- submitClient.php
+++ submitClient.php
@@ -2,36 +2,8 @@
 ini_set('display_errors', 'On');
 error_reporting(E_ALL | E_STRICT);
 if($_POST){
-$database_name = "eventDB";
-$mysql_host = "nmbzrmx555.database.windows.net"; //almost always 'localhost'
-$database_user = "eecs";
-$database_pwd = "IntelNUC777";
-//$dbc = OpenConnection();
-try
-    {
-        $serverName = "tcp:nmbzrmx555.database.windows.net,1433";
-        $connectionOptions = array("Database"=>"eventDB",
-            "Uid"=>"eecs", "PWD"=>"IntelNUC777");
-        $conn = sqlsrv_connect($serverName, $connectionOptions);
-        if($conn == false)
-            die(FormatErrors(sqlsrv_errors()));
-    }
-    catch(Exception $e)
-    {
-        echo("Error!");
-    }
-echo('hi');
-if(!$conn)
-{
-    die("We are currently experiencing very heavy traffic to our site, please be patient and try again shortly.");
-}
-$db = mssql_select_db($database_name);
-if(!$db)
-{
-    die("Failed to connect to database - check your database name.");
-}
-$sql = "insert into client(clientName, clientAreaCode, eventName, clientPhone, eventID)
-values('".$_POST['name']."', '".$_POST['area_code']."', '".$_POST['eventName']."', '".$_POST['phone_number']."', '".$_POST['eventID']."');";
+ReadData();
+$sql = ""
 $res = mssql_query($sql);
 
 echo 'Thank you for your entry!';
@@ -40,14 +12,38 @@ else
 {
 	echo 'Not POST';
 }
-
+function ReadData()
+{
+    try
+    {
+        $conn = OpenConnection();
+        $tsql = "insert into client(clientName, clientAreaCode, eventName, clientPhone, eventID)
+values('".$_POST['name']."', '".$_POST['area_code']."', '".$_POST['eventName']."', '".$_POST['phone_number']."', '".$_POST['eventID']."');";
+        $getProducts = sqlsrv_query($conn, $tsql);
+        if ($getProducts == FALSE)
+            die(FormatErrors(sqlsrv_errors()));
+        $productCount = 0;
+        while($row = sqlsrv_fetch_array($getProducts, SQLSRV_FETCH_ASSOC))
+        {
+            echo($row['CompanyName']);
+            echo("<br/>");
+            $productCount++;
+        }
+        sqlsrv_free_stmt($getProducts);
+        sqlsrv_close($conn);
+    }
+    catch(Exception $e)
+    {
+        echo("Error!");
+    }
+}
 function OpenConnection()
 {
     try
     {
         $serverName = "tcp:nmbzrmx555.database.windows.net,1433";
         $connectionOptions = array("Database"=>"eventDB",
-            "Uid"=>"eecs@nmbzrmx555", "PWD"=>"IntelNUC777");
+            "Uid"=>"eecs", "PWD"=>"IntelNUC777");
         $conn = sqlsrv_connect($serverName, $connectionOptions);
         if($conn == false)
             die(FormatErrors(sqlsrv_errors()));
